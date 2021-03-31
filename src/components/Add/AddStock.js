19136c4f92783c27e780c01ebadc9b65b11335dd
Arpon Purkayastha <arponp30@gmail.com>
2021-03-31 14:57:13 -0700

    implement adding stocks to portfolio

--- src/components/Add/AddStock.js
+++ src/components/Add/AddStock.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import UserContext from "../../user/UserContext";
 
 function AddStock({ stock }) {
+  const { user } = useContext(UserContext);
+  const [quantity, setQuantity] = useState(0);
+  const [error, setError] = useState(null);
   const handleSubmit = async (e) => {
+    console.log(quantity);
     e.preventDefault();
+    const stocks = [{ symbol: stock.symbol, quantity }];
+    console.log(stocks);
+    const { status } = await axios.post(
+      "http://localhost:4000/portfolio",
+      { stocks: stocks },
+      {
+        headers: { authorization: `Bearer ${user.token}` },
+      }
+    );
+    status == 201 ? setError("Successfully Added") : setError("Cannot Add");
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <h3>{stock.name.toLowerCase().toUpperCase()}</h3>
+        <h3>
+          {stock.name
+            .toLowerCase()
+            .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))}
+        </h3>
         <p>{stock.symbol}</p>
-        <p>{stock.last}</p>
-        <input placeholder="Quantity" name="quantity" />
+        <p>Price: {stock.last.toFixed(2)}</p>
+        <input
+          placeholder="Quantity"
+          name="quantity"
+          onChange={(e) => setQuantity(e.target.value)}
+        />
         <div />
-        <input type="submit" />
+        <button type="submit">Add</button>
+        <div>{error}</div>
       </form>
     </div>
   );
